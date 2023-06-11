39452f72a4d6a0125e8de97f5c615719057ff0db
数星星的少年郎 <tzg1064690854@gmail.com>
2023-06-11 18:16:32 +0800

    调整热加载

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # SharedFile-Ktor
 跨平台上传下载文件
 ## 使用方法
-1. 将需要分享的文件放在resources/share目录下面
-2. 上传文件路径会在build/resources/share下面
+1. 在项目resources目录下，新建share文件夹
+2. 将需要分享的文件放在resources/share目录下面
+3. 上传文件路径会在build/resources/share下面
 
 ## Tips
 这项目是我学习与练习Ktor的，仅供参考，切勿投入商业使用，素材版权问题概不负责，谢谢合作
--- src/main/kotlin/com/toowhite/Application.kt
+++ src/main/kotlin/com/toowhite/Application.kt
@@ -6,13 +6,12 @@ import io.ktor.server.netty.*
 import com.toowhite.plugins.*
 
 fun main() {
-    // ./gradlew -t installDist  开启auto-load需要 监听变更
     embeddedServer(
         Netty,
         port = 8000,
         host = "0.0.0.0",
         module = Application::module,
-        watchPaths = listOf("myshare-ktor")
+        watchPaths = listOf("SharedFile-Ktor") // Auto reload 配置 并执行命令./gradlew -t installDist  开启auto-load需要 监听变更
     ).start(wait = true)
 }
 
